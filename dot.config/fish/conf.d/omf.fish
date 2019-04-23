2c81bec6a3f571e8bd32fff4061200ab15346c05
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2019-04-23 22:47:35 +0400

    Eval omf init file only if it's available

--- dot.config/fish/conf.d/omf.fish
+++ dot.config/fish/conf.d/omf.fish
@@ -4,4 +4,6 @@ set -q XDG_DATA_HOME
   or set -gx OMF_PATH "$HOME/.local/share/omf"
 
 # Load Oh My Fish configuration.
-source $OMF_PATH/init.fish
+if test -d $OMF_PATH
+  source $OMF_PATH/init.fish
+end
