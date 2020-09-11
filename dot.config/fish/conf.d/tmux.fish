fea3f618489d48c5f2069c2b681a5b67b36e7c2b
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2020-09-11 12:52:08 +0200

    Add `tbase` function to run basic tmux swiftly
    Plus, a couple new vim plugins

---
+++ dot.config/fish/conf.d/tmux.fish
@@ -0,0 +1,13 @@
+set normal (set_color normal)
+set red (set_color -o red)
+
+function tbase --description="Initiates a base tmux session (or attaches it)"
+  # see if we are inside tmux session already
+  if set -q TMUX
+    printf "$red\e0Cannot make this happen: already in a tmux session\n$normal"
+    return
+  end
+
+  # create or attach to a session named as current user
+  tmux new -A -s "$USER"
+end
