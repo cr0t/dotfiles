e55218c4135ada170658454340e5a311600ab516
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2021-05-27 19:41:08 +0200

    Add Elixir escripts path to run installed ones
    Done that during Livebook server installation. See details here: https://github.com/elixir-nx/livebook#escript

--- dot.config/fish/conf.d/paths.fish
+++ dot.config/fish/conf.d/paths.fish
@@ -1,9 +1,10 @@
 set --local lo_sbin "/usr/local/sbin"
 set --local littles "$HOME/.dotfiles/littles/"
 set --local vs_code "/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+set --local escripts "$HOME/.mix/escripts"
 
 # Check if path exist and add it to the $PATH
-set --local paths $lo_sbin $littles $vs_code
+set --local paths $lo_sbin $littles $vs_code $escripts
 
 for path in $paths
     if test -d $path
