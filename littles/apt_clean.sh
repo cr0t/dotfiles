c7e83f4f7a44526215d68c360718c3438280db9c
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2021-05-31 21:49:16 +0200

    Remove redundant messages in apt_* scripts

--- littles/apt_clean.sh
+++ littles/apt_clean.sh
@@ -5,7 +5,6 @@ UNAME=$(uname | tr "[:upper:]" "[:lower:]")
 
 if [ "$UNAME" == "linux" ]; then
   sudo apt autoclean && sudo apt autoremove
-  echo "Cleaning is done!"
 else
   echo "You're not in Linux, you're in $UNAME"
 fi
--- littles/apt_update.sh
+++ littles/apt_update.sh
@@ -5,7 +5,6 @@ UNAME=$(uname | tr "[:upper:]" "[:lower:]")
 
 if [ "$UNAME" == "linux" ]; then
   sudo apt update && sudo apt dist-upgrade -y
-  echo "Consider to restart to get the most fresh system booted up!"
 else
   echo "You're not in Linux, you're in $UNAME"
 fi
