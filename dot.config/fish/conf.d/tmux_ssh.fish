78ca9c592861bb083f97056cb2c207c86b0bf489
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2021-08-12 16:27:26 +0200

    Fix tmux_ssh pane name function
    We noticed that our fish `ssh`-proxy function was buggy and didn't allow
    us to use any ssh command line arguments (for example, it was failing on
    `ssh -X example.com` call).
    
    We found another way of extracting domain name from the very last
    parameter of arguments list.

--- dot.config/fish/conf.d/tmux_ssh.fish
+++ dot.config/fish/conf.d/tmux_ssh.fish
@@ -1,9 +1,13 @@
 function ssh --description="Renames current tmux window to remote hostname"
     if set -q TMUX
-        tmux rename-window (echo $argv | cut -d . -f 1)
-        command ssh "$argv"
+        # use last parameter as domain name, for example from `ssh -v summercode.com`
+        # it will retrieve `summercode` and assign to $ssh_host
+        set --local ssh_host (echo $argv | rev | cut -d ' ' -f 1 | rev | cut -d '.' -f 1)
+
+        tmux rename-window $ssh_host
+        command ssh $argv
         tmux set-window-option automatic-rename on 1>/dev/null
     else
-        command ssh "$argv"
+        command ssh $argv
     end
 end
