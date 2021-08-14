dd9f1651b779fdf3235c9866110e26ddd744bcda
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2021-08-14 12:45:10 +0200

    Use ~/.local/bin instead of ~/.littles.local
    Some of systems use ~/.local/bin directory as it's local storage for
    small user-owned scripts and executables, so we think it's better to use
    something more or less standard.

--- dot.config/fish/conf.d/paths.fish
+++ dot.config/fish/conf.d/paths.fish
@@ -2,10 +2,10 @@ set --local lo_sbin "/usr/local/sbin"
 set --local littles "$HOME/.dotfiles/littles/"
 set --local vs_code "/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
 set --local escripts "$HOME/.mix/escripts"
-set --local lo_littles "$HOME/.littles.local"
+set --local lo_bin "$HOME/.local/bin"
 
 # Check if path exist and add it to the $PATH
-set --local paths $lo_sbin $littles $vs_code $escripts $lo_littles
+set --local paths $lo_sbin $littles $vs_code $escripts $lo_bin
 
 for path in $paths
     if test -d $path
