703b3d86d2a3895670afe5bcae300a096a3b9cc6
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2021-08-14 10:35:42 +0200

    Fix fish fzf integration
    Use `fd` with a list of additional default arguments when press CTRL+T
    in fish session.

--- dot.config/fish/conf.d/fzf.fish
+++ dot.config/fish/conf.d/fzf.fish
@@ -1,5 +1,8 @@
-# use fd that will respect gitignore and show hidden filesm and follow symbolic links
-set -gx FZF_DEFAULT_COMMAND 'fd --type f --hidden --follow --exclude .git'
+# IMPORTANT: don't forget to install fzf and fish key bindings! See https://github.com/junegunn/fzf#installation
+
+# use fd that will respect gitignore, ignore node_modules and show hidden files and follow symbolic links
+set -gx FZF_DEFAULT_COMMAND 'fd --type f --hidden --follow --exclude .git --exclude node_modules'
+set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
 
 function fzf-preview --description="Shortcut to run fzf with --preview option params"
     fzf --preview "bat --style=numbers --color=always --line-range :500 {}"
