2020b37ecb3a20a1db37f55b7667d730ef1bca00
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2021-08-17 14:57:41 +0200

    Improve fzf shell window layout and styles

--- dot.config/fish/conf.d/fzf.fish
+++ dot.config/fish/conf.d/fzf.fish
@@ -1,9 +1,15 @@
-# IMPORTANT: don't forget to install fzf and fish key bindings! See https://github.com/junegunn/fzf#installation
+# IMPORTANT: don't forget to install bat, fd, fzf and fish key bindings;
+# see https://github.com/junegunn/fzf#installation
 
 # use fd that will respect gitignore, ignore node_modules and show hidden files and follow symbolic links
 set -gx FZF_DEFAULT_COMMAND 'fd --type f --hidden --follow --exclude .git --exclude node_modules'
 set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
 
-function fzfpreview --description="Shortcut to run fzf with --preview option params"
-    fzf --preview "bat --style=numbers --color=always --line-range :500 {}"
+# a bit of styling
+#set -g fzf_preview_command 'file {}' # prints name and type of selected file
+set -g fzf_preview_command 'bat --style numbers --color always --line-range :256 {}'
+set -gx FZF_DEFAULT_OPTS "--height 50% --layout reverse --border --inline-info --preview '$fzf_preview_command'"
+
+function fzfpreview --description='Shortcut to run fzf with --preview option params'
+    fzf --preview "$fzf_preview_command"
 end
