b85e6bfa59fb600c186301546988f16d3edda58b
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2022-08-11 11:26:22 +0200

    Add 'spell' (wordnet + fzf) command to PATH list

--- dot.config/fish/conf.d/paths.fish
+++ dot.config/fish/conf.d/paths.fish
@@ -3,9 +3,10 @@ set --local littles "$HOME/.dotfiles/littles/"
 set --local vs_code "/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
 set --local escripts "$HOME/.mix/escripts"
 set --local lo_bin "$HOME/.local/bin"
+set --local wordnet "$HOME/.vim/bundle/fzf-wordnet.vim/bin"
 
 # Check if path exist and add it to the $PATH
-set --local paths $lo_sbin $littles $vs_code $escripts $lo_bin
+set --local paths $lo_sbin $littles $vs_code $escripts $lo_bin $wordnet
 
 for path in $paths
     if test -d $path
