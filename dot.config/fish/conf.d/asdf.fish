ebfaf04f282ceb2e2c5e24a6c37b6495403e09ec
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2023-01-20 22:54:51 +0100

    Initialize asdf in Ubuntu

--- dot.config/fish/conf.d/asdf.fish
+++ dot.config/fish/conf.d/asdf.fish
@@ -1,5 +1,11 @@
 if type -q brew
+    # macOS
     if test -f (brew --prefix asdf)"/libexec/asdf.fish"
         source (brew --prefix asdf)"/libexec/asdf.fish"
+    end
+else
+    # Ubuntu
+    if test -f ~/.asdf/asdf.fish
+      source ~/.asdf/asdf.fish
     end
 end
