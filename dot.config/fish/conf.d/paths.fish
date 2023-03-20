a8b6d0fcff026c09f6d78ef7175b920085568730
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2023-03-20 22:47:37 +0100

    Remove Vim's wordnet plugin (as I don't use it)

--- dot.config/fish/conf.d/paths.fish
+++ dot.config/fish/conf.d/paths.fish
@@ -3,13 +3,13 @@ set --local littles "$HOME/.dotfiles/littles/"
 set --local vs_code "/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
 set --local escripts "$HOME/.mix/escripts"
 set --local lo_bin "$HOME/.local/bin"
-set --local wordnet "$HOME/.vim/bundle/fzf-wordnet.vim/bin"
 
 # Check if path exist and add it to the $PATH
-set --local paths $lo_sbin $littles $vs_code $escripts $lo_bin $wordnet
+set --local paths $lo_sbin $littles $vs_code $escripts $lo_bin
 
 for path in $paths
     if test -d $path
-        set -gx PATH $path $PATH
+        # Docs: https://fishshell.com/docs/current/cmds/fish_add_path.html
+        fish_add_path --path $path
     end
 end
