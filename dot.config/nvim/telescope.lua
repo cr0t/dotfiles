9015c1854857097ee02169bb0a1e1b2945207527
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2023-03-21 10:48:22 +0100

    Adjust nvim's Telescope layout to my preferences

--- dot.config/nvim/telescope.lua
+++ dot.config/nvim/telescope.lua
@@ -6,12 +6,28 @@ vim.keymap.set('n', '<leader>f', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>h', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>b', builtin.buffers, {})
 
+-- The following Telescope settings configures it to layout similar to this:
+--
+-- ┌────────────────────────────────────────────────┐
+-- │   ┌───────────────────┐┌──────Filepath─────┐   │
+-- │   │> Prompt           ││                   │   │
+-- │   └───────────────────┘│                   │   │
+-- │   ┌───────────────────┐│                   │   │
+-- │   │  Results          ││      Preview      │   │
+-- │   │  Results          ││                   │   │
+-- │   │  ...              ││                   │   │
+-- │   │                   ││                   │   │
+-- │   └───────────────────┘└───────────────────┘   │
+-- └────────────────────────────────────────────────┘
 require('telescope').setup {
   defaults = {
+    dynamic_preview_title = true,
+    layout_strategy = 'flex',
+    layout_config = { prompt_position = "top" },
+    sorting_strategy = "ascending",
     mappings = {
-      i = {
-        ["<esc>"] = actions.close,
-      }
-    }
+      i = { ["<esc>"] = actions.close } -- press ESC once to close the modal
+    },
+    winblend = 5, -- pseudo-transparency for the modal (0..100)
   },
 }
