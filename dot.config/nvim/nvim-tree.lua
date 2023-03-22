26d81fcdf77c0cce4eeda3c1ea260ae4c7c3dacf
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2023-03-22 23:19:03 +0100

    Clean up nvim-tree settings (to mimic my NERDtree)
    ...and remove unnecessary NERDtree files and plugin installation.

--- dot.config/nvim/nvim-tree.lua
+++ dot.config/nvim/nvim-tree.lua
@@ -11,39 +11,25 @@ local function opts(desc)
   return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
 end
 
+-- show/hide the tree panel
 local function treeToggle()
-  api.tree.toggle({focus = true })
+  api.tree.toggle({focus = true})
 end
 
-local function treeFind()
-  api.tree.toggle({focus = true, find_file = true})
+-- my preference is to always open (or leave it opened) when run "find file"
+local function treeFindFile()
+  api.tree.open({focus = true, find_file = true})
 end
 
+-- my minimum basic functionality (migrated from NERDtree)
 vim.keymap.set('n', '<C-b>', treeToggle, opts('Toggle Tree'))
-vim.keymap.set('n', '<C-f>', treeFind, opts('Find File'))
+vim.keymap.set('n', '<C-f>', treeFindFile, opts('Find File'))
 
+-- close on quit
 vim.api.nvim_create_autocmd({'QuitPre'}, {
   callback = function()
     vim.cmd("NvimTreeClose")
   end,
 })
 
--- empty setup using defaults
-require('nvim-tree').setup({
-  sort_by = 'case_sensitive',
-  update_focused_file = {
-	  enable = true,
-  },
-})
-
--- OR setup with some options
---require('nvim-tree').setup({
---  
---  renderer = {
---    group_empty = true,
---  },
---  filters = {
---    dotfiles = true,
---  },
---})
-
+require('nvim-tree').setup({})
