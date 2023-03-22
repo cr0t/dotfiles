409a533b831589676679f1e6541f66d5a9f4489a
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2023-03-22 21:02:37 +0100

    Update fzf mini-helpers (fish functions) settings
    Rename too long `fzfpreview` to simple `fzp`, add `rgp` based on ripgrep
    utility. We can quickly open files found by these two helpers by
    executing this, for example: `vim $(rgp)`.
    
    We have also added additional checks for availability of `fzf` and `rg`
    commands.

--- dot.config/fish/conf.d/fzf.fish
+++ dot.config/fish/conf.d/fzf.fish
@@ -1,15 +1,39 @@
-# IMPORTANT: don't forget to install bat, fd, fzf and fish key bindings;
-# see https://github.com/junegunn/fzf#installation
+# IMPORTANT: install bat, fd, fzf and fish key bindings to improve quality of your life with fzf
+#
+# - https://github.com/junegunn/fzf#installation
+# - https://github.com/junegunn/fzf/blob/master/ADVANCED.md
+# - https://andrew-quinn.me/fzf/
 
-# use fd that will respect gitignore, ignore node_modules and show hidden files and follow symbolic links
-set -gx FZF_DEFAULT_COMMAND 'fd --type f --hidden --follow --exclude .git --exclude node_modules'
+if type -q fd
+  # use fd, plus set it to respect gitignore, ignore node_modules, show hidden files, and follow symbolic links
+  set -gx FZF_DEFAULT_COMMAND 'fd --type f --hidden --follow --exclude .git --exclude node_modules'
+else
+  set -gx FZF_DEFAULT_COMMAND 'find * -type f'
+end
+
 set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
 
-# a bit of styling
-#set -g fzf_preview_command 'file {}' # prints name and type of selected file
-set -g fzf_preview_command 'bat --style numbers,changes --color always --line-range :256 {}'
+if type -q bat
+  # a bit of styling
+  set -g fzf_preview_command 'bat --style numbers,changes --color always --line-range :256 {}'
+else
+  set -g fzf_preview_command 'cat {}'
+end
+
 set -gx FZF_DEFAULT_OPTS "--height 70% --layout reverse --border --inline-info --preview '$fzf_preview_command'"
 
-function fzfpreview --description='Shortcut to run fzf with --preview option params'
-    fzf --preview "$fzf_preview_command"
+function fzp --description='Shortcut to run fzf with --preview option params'
+    if type -q fzf
+        fzf --preview "$fzf_preview_command"
+    else
+        echo "fzf is not available"
+    end
+end
+
+function rgp --description='Shortcut to run rigpgrep in local directory and search in the output via fzf'
+    if type -q rg
+        rg . | fzf --no-preview | cut -d ":" -f 1
+    else
+        echo "rigpgrep is not available"
+    end
 end
