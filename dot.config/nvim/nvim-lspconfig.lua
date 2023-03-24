9e236c97e182b127da6fcb5634c9f4318c6fd431
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2023-03-24 23:46:03 +0100

    Add autocompletion plugins (nvim-cmp as the base one)

--- dot.config/nvim/nvim-lspconfig.lua
+++ dot.config/nvim/nvim-lspconfig.lua
@@ -1,8 +1,9 @@
 local lspconfig = require('lspconfig')
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 lspconfig.elixirls.setup {
   cmd = { vim.fn.expand('~/.local/lsp/elixir-ls/language_server.sh') },
-  --capabilities = capabilities,
+  capabilities = capabilities,
   settings = {
     elixirLS = {
       dialyzerEnabled = true,
