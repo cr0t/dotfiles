07234d12374b1c35aaa88b2d192d0b9b447b3750
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2023-09-25 22:49:32 +0200

    Add alias to dockerized `kamal` deployment utility

---
+++ dot.config/fish/conf.d/kamal.fish
@@ -0,0 +1,3 @@
+function kamal --description="Dockerized kamal deployment utility"
+    docker run -it --rm -v "$PWD:/workdir" -v '/run/host-services/ssh-auth.sock:/run/host-services/ssh-auth.sock' -e SSH_AUTH_SOCK='/run/host-services/ssh-auth.sock' -v /var/run/docker.sock:/var/run/docker.sock ghcr.io/basecamp/kamal:latest $argv
+end
