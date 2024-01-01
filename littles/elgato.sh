e45cf9361d3adc65dd6ee399fae8546be8d3da04
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2024-01-01 22:16:34 +0100

    Update elgato.sh, clean it a bit

--- littles/elgato.sh
+++ littles/elgato.sh
@@ -1,34 +1,42 @@
 #!/bin/bash
 
-# This macOS-only script supposed to run in background all the time: it automatically switches
+set -euo pipefail
+
+# This (macOS-only!) script supposed to run in background all the time: it automatically switches
 # on and off configured Elgato lights.
 #
-# To make it work we have to provide a few settings values.
+# The original idea is picked up here: https://github.com/adamesch/elgato-key-light-api
 #
-# P.S. The original idea is picked up here: https://github.com/adamesch/elgato-key-light-api
+# To make it work we have to provide a few settings:
 
 TEMPERATURE=4250 # 2900–6950K
 BRIGHTNESS=33 # 1–100
-DEVICES=(192.168.1.90 192.168.1.91 192.168.1.93)
+DEVICES_IPS=(192.168.1.90 192.168.1.91 192.168.1.93)
+
+# TODO: Move devices IP-addresses list to some file.
 
 # Converts a color temperature (in K) to the format Elgato Lights like (e.g., integer from 143–344)
-# Warning: the actual precision is not ideal. Maybe we need to find a better way on how to convert
+#
+# NOTE: the actual precision is not ideal. Likely, we need to find a better way on how to convert
 # the color temperatue.
 function kelvin_to_elgato() {
   echo "" | awk "END {print int((987007 * $1 ^ -0.999)) }"
 }
 
+# Builds a full API url to the light using the given IP address
 function api_url() {
   echo "http://$1:9123/elgato/lights"
 }
 
+# Uses `curl` to send the given data to the given url using PUT-request.
 function send_put() {
   URL=$1
   DATA=$2
 
-  curl --location --request PUT $URL --header 'Content-Type: application/json' --data-raw $DATA
+  curl --silent --output /dev/null --request PUT $URL --header 'Content-Type: application/json' --data-raw $DATA
 }
 
+# Switches the given light on
 function on() {
   URL=$(api_url $1)
   TEMP=$(kelvin_to_elgato $2)
@@ -39,6 +47,7 @@ function on() {
   send_put $URL $DATA
 }
 
+# Switches the given light off
 function off() {
   URL=$(api_url $1)
 
@@ -47,35 +56,34 @@ function off() {
   send_put $URL $DATA
 }
 
+# ...camera, action!
 function lights_on() {
-  for light in "${DEVICES[@]}"
-  do
+  for light in "${DEVICES_IPS[@]}"; do
     on $light $TEMPERATURE $BRIGHTNESS
   done
 }
 
+# ...cut!
 function lights_off() {
-  for light in "${DEVICES[@]}"
-  do
+  for light in "${DEVICES_IPS[@]}"; do
     off $light
   done
 }
 
-# DEBUG
-#
-# on "192.168.1.90" $TEMPERATURE $BRIGHTNESS
-# off "192.168.1.90"
+# Here is the main piece of the functionality of this script: we use `log` command and predicate
+# option to filter camera on and off events. We take the input and switch on or off the ligths
+# configured at the top of this file.
 
-log stream --predicate 'subsystem == "com.apple.UVCExtension" AND composedMessage CONTAINS "Post PowerLog"' | while read line; do
-  if echo "$line" | grep -q "= On"; then
-    echo "Camera has been activated"
+CAMERA_EVENTS='subsystem == "com.apple.UVCExtension" AND composedMessage CONTAINS "Post PowerLog"'
 
+log stream --predicate "$CAMERA_EVENTS" | while read LOGLINE; do
+  if [[ $LOGLINE == *"= On"* ]]; then
+    # echo "Camera has been activated"
     lights_on
   fi
 
-  if echo "$line" | grep -q "= Off"; then
-    echo "Camera has been deactivated"
-
+  if [[ $LOGLINE == *"= Off"* ]]; then
+    # echo "Camera has been deactivated"
     lights_off
   fi
 done
