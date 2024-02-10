7b9ad19924ed5e049e9fee4f3ebc85c8338aa501
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2024-02-10 21:30:59 +0100

    Add KERL_* env variables to erlang.fish
    It's easy to forget to run the full command when installing Erlang
    runtime via asdf, which is:
    
    env KERL_BUILD_DOCS=yes KERL_CONFIGURE_OPTIONS="--without-javac" asdf install erlang <version>
    
    So, it's better to set these variables in the fish configuration file
    and use them by default.
    
    They tell compiler to build the Erlang docs (to be used in IEx sessions,
    for example), and to skip searching for JAVA compiler (this might be an
      issue in some cases, but we are not aware of such yet).

--- dot.config/fish/conf.d/erlang.fish
+++ dot.config/fish/conf.d/erlang.fish
@@ -1,1 +1,5 @@
 set -gx ERL_AFLAGS "-kernel shell_history enabled"
+
+# next two used to build Erlang (with asdf install erlang ..., for example)
+set -gx KERL_BUILD_DOCS "yes"
+set -gx KERL_CONFIGURE_OPTIONS "--without-javac"
