160f7f3851fe96e73bb939829ae511c2302bbd72
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2024-06-23 12:22:24 +0200

    Adjust fonts for Alacritty and Neovide

--- dot.config/nvim/neovide.lua
+++ dot.config/nvim/neovide.lua
@@ -1,6 +1,6 @@
 if vim.g.neovide then
   -- See https://neovide.dev/configuration.html for more options
-  vim.o.guifont = "MesloLGS Nerd Font:h14"
+  vim.o.guifont = "MesloLGS Nerd Font:h13"
   vim.g.neovide_cursor_animation_length = 0
   vim.g.neovide_scroll_animation_length = 0
 end
