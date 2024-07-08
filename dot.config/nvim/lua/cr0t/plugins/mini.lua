cb6c601959678ccd3f7c6e01bb5dcd4f83e37d53
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2024-07-08 20:06:06 +0200

    Add cool starter screen logo of StackFusion

--- dot.config/nvim/lua/cr0t/plugins/mini.lua
+++ dot.config/nvim/lua/cr0t/plugins/mini.lua
@@ -3,8 +3,21 @@ return {
         "echasnovski/mini.nvim",
         version = false,
         config = function()
-            require("mini.statusline").setup()
+            -- Check out these resources to make something cool:
+            -- https://texteditor.com/ascii-art/ or https://texteditor.com/multiline-text-art/
+            local logo = table.concat({
+                ".▄▄ · ▄▄▄▄▄▄ ▄▄▄·  ▄▄· ▄ •▄ ·▄▄▄▄• ▄▌.▄▄ · ▪         ▐ ▄ ",
+                "▐█ ▀. ▀•██ ▀▐█ ▀█ ▐█ ▌▪█▌▄▌▪█  ·█▪██▌▐█ ▀. ██  ▄█▀▄ •█▌▐█",
+                "▄▀▀▀█▄  ▐█.▪▄█▀▀█ ██ ▄▄▐▀▀▄·█▀▀▪█▌▐█▌▄▀▀▀█▄▐█·▐█▌.▐▌▐█▐▐▌",
+                "▐█▄▪▐█  ▐█▌·▐█▪ ▐▌▐███▌▐█.█▌██ .▐█▄█▌▐█▄▪▐█▐█▌▐█▌.▐▌██▐█▌",
+                " ▀▀▀▀   ▀▀▀  ▀  ▀ ·▀▀▀ ·▀  ▀▀▀▀  ▀▀▀  ▀▀▀▀ ▀▀▀ ▀█▄▀▪▀▀ █▪",
+            }, "\n")
+
             require("mini.comment").setup()
+            require("mini.starter").setup({
+                header = logo
+            })
+            require("mini.statusline").setup()
             require("mini.pairs").setup()
             require("mini.tabline").setup()
             require("mini.trailspace").setup({
