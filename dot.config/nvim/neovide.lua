1bdd92365bacb6044b0ede5ffc447c69d349dc6f
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2024-07-10 22:56:22 +0200

    Re-start Neovim's (and partially, Vim's) configs
    During the recent experiements with new Neovim plugins and
    configurations, we thought that it would be interesting (and, probably,
    useful) to replace Vim's Plug with modern plugin manager (like Lazy), as
    well as to cleanup some old Vim's settings that we do not use.
    
    So... This is the beginning of the job: we have removed most of the old
    Lua-files used by Neovim, returned some key bindings (and will use them
    both in Vim and Neovim), made some cleanups to the .vimrc.
    
    Let's how it will end up.

--- dot.config/nvim/neovide.lua
+++
@@ -1,6 +0,0 @@
-if vim.g.neovide then
-  -- See https://neovide.dev/configuration.html for more options
-  vim.o.guifont = "MesloLGS Nerd Font:h13"
-  vim.g.neovide_cursor_animation_length = 0
-  vim.g.neovide_scroll_animation_length = 0
-end
