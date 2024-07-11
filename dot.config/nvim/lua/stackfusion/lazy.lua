14957e1935387025ee25de3c14b939743422b183
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2024-07-11 23:00:25 +0200

    Remove helpers/path.lua and replace it in lazy.lua

--- dot.config/nvim/lua/stackfusion/lazy.lua
+++ dot.config/nvim/lua/stackfusion/lazy.lua
@@ -1,23 +1,23 @@
-local path = require("stackfusion.helpers.path")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazyrepo = "https://github.com/folke/lazy.nvim.git"
 
-local lazyroot = path.join(path.dataroot, "lazy")
-local lazypath = path.join(lazyroot, "lazy.nvim")
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
 
-if not path.exists(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "--branch=stable",
-        "https://github.com/folke/lazy.nvim.git",
-        lazypath
-    })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out,                            "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 
 vim.opt.runtimepath:prepend(lazypath)
 
 require("lazy").setup("stackfusion.plugins", {
-    root = lazyroot,
     performance = {
         rtp = {
             disabled_plugins = {
