d80f9f425f5762eea0e460e0a1c13c5eea329a6f
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2024-07-13 23:00:31 +0200

    Add mini.nvim, set logo, new shortcut to trim

--- dot.config/nvim/lua/stackfusion/config/keymaps.lua
+++ dot.config/nvim/lua/stackfusion/config/keymaps.lua
@@ -1,4 +1,11 @@
 local wk = require("which-key")
+local miniMap = require("mini.map")
+local miniTrail = require("mini.trailspace")
+
+local function trim()
+    miniTrail.trim()
+    miniTrail.trim_last_lines()
+end
 
 wk.add({
     -- Foremost, provide descriptions to Vim's shortcuts (we set in vimrc.keymap)
@@ -12,4 +19,6 @@ wk.add({
     -- Now it's time to add some extra - Neovim-related only
     { "<leader>L", "<cmd>Lazy<cr>",               desc = "Lazy.nvim" },
     { "<leader>M", "<cmd>Mason<cr>",              desc = "Mason.nvim" },
+    { "<leader>m", miniMap.toggle,                desc = "Toggle mini-map" },
+    { "<leader>t", trim,                          desc = "Trim whitespace" },
 })
