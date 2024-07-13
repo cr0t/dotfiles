2fae52a465db9014d4bd58b7b2a85c491078b7dc
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2024-07-14 00:02:42 +0200

    Bring IDE-ish shortcuts: gD, <space>f, etc.

---
+++ dot.config/nvim/lua/stackfusion/config/diagnostics.lua
@@ -0,0 +1,21 @@
+-- Diagnostics
+-- -----------
+--
+-- Show diagnostics warnings automatically in a floating window, we used this guide initially, but had to rework it:
+-- https://github.com/neovim/nvim-lspconfig/wiki/UI-Customization#show-line-diagnostics-automatically-in-hover-window
+
+vim.diagnostic.config({ virtual_text = false })
+vim.cmd([[ autocmd ColorScheme * highlight NormalFloat guibg=none ]])
+vim.api.nvim_create_autocmd("CursorHold", {
+    desc = "Shows diagnostics floating window in normal mode when cursor holds on a line with an issue",
+    callback = function()
+        local opts = {
+            focusable = false,
+            close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+            border = "single",
+            source = "if_many",
+        }
+
+        vim.diagnostic.open_float(nil, opts)
+    end
+})
