77035085e4de15961a8fff09a30aba038986e5d5
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2024-08-09 20:23:28 +0200

    Ignore tree-sitter parser for javascript...
    ...because it gives weird errors when touching JS-files.

--- dot.config/nvim/lua/stackfusion/config/highlighting.lua
+++ dot.config/nvim/lua/stackfusion/config/highlighting.lua
@@ -1,5 +1,6 @@
 require("nvim-treesitter.configs").setup({
   auto_install = vim.fn.executable "git" == 1 and vim.fn.executable "tree-sitter" == 1, -- only if git and tree-sitter available
+  ignore_install = { "javascript" },
   ensure_installed = {
     "bash", "c", "diff", "elixir", "erlang", "fish", "heex", "html", "javascript", "jsdoc", "json", "jsonc", "lua",
     "luadoc", "luap", "markdown", "markdown_inline", "python", "query", "regex", "ruby", "svelte", "toml", "tsx",
