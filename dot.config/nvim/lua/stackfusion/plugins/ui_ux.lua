ac679eb1285373edb5e22f429614f3455d063be4
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2024-08-10 12:56:59 +0200

    Actual fix for bufferline.nvim UX when opening...
    ...a single file or editor for `git commit` command.
    
    "VeryLazy" loading does a bad job for initialization of bufferline when
    opening a single file, so let's change the way we load this plugin.

--- dot.config/nvim/lua/stackfusion/plugins/ui_ux.lua
+++ dot.config/nvim/lua/stackfusion/plugins/ui_ux.lua
@@ -7,13 +7,13 @@ return {
     "akinsho/bufferline.nvim",
     version = "*",
     dependencies = "nvim-tree/nvim-web-devicons",
-    event = "VeryLazy",
+    event = { "BufReadPre", "BufNewFile" },
+    lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
     opts = function()
       local bufferline = require("bufferline")
 
       local opts = {
         options = {
-          auto_toggle_bufferline = true,
           diagnostics = "nvim_lsp",
           color_icons = false,
           indicator = { style = "none" },
