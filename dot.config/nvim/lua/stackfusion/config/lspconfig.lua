407ba40fbd10e7781d2c773177ee289ee037aa57
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2024-09-06 22:37:49 +0200

    Add tsserver => ts_ls workaround for nvim-lspconfig

--- dot.config/nvim/lua/stackfusion/config/lspconfig.lua
+++ dot.config/nvim/lua/stackfusion/config/lspconfig.lua
@@ -9,6 +9,11 @@ require("mason-lspconfig").setup_handlers({
   -- The first entry (without a key) will be the default handler and will be called for each installed server that
   -- doesn't have a dedicated handler.
   function(server_name)
+    -- tsserver renamed to ts_ls: https://github.com/neovim/nvim-lspconfig/pull/3232
+    if server_name == "tsserver" then
+      server_name = "ts_ls"
+    end
+
     require("lspconfig")[server_name].setup {
       capabilities = require("cmp_nvim_lsp").default_capabilities()
     }
