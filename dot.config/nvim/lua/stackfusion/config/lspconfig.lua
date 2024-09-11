48cd7eb7cfc0db270cac58e94299b5c94c6c7be3
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2024-09-11 22:07:46 +0200

    Remove recent tsserver vs. ts_ls workaround...
    ...because mason-lspconfig.nvim got updated and fixed the issue.

--- dot.config/nvim/lua/stackfusion/config/lspconfig.lua
+++ dot.config/nvim/lua/stackfusion/config/lspconfig.lua
@@ -1,7 +1,7 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
   ensure_installed = {
-    "ansiblels", "bashls", "cssls", "elixirls", "elmls", "html", "lua_ls", "solargraph", "svelte", "tsserver", "yamlls"
+    "ansiblels", "bashls", "cssls", "elixirls", "elmls", "html", "lua_ls", "solargraph", "svelte", "ts_ls", "yamlls"
   },
 })
 
@@ -9,11 +9,6 @@ require("mason-lspconfig").setup_handlers({
   -- The first entry (without a key) will be the default handler and will be called for each installed server that
   -- doesn't have a dedicated handler.
   function(server_name)
-    -- tsserver renamed to ts_ls: https://github.com/neovim/nvim-lspconfig/pull/3232
-    if server_name == "tsserver" then
-      server_name = "ts_ls"
-    end
-
     require("lspconfig")[server_name].setup {
       capabilities = require("cmp_nvim_lsp").default_capabilities()
     }
