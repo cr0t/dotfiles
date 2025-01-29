42982a69fcb78e7e7cfdf2515734ef6db94034e5
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2025-01-29 10:40:09 +0100

    Fix Neovim's syntax highlighting for .heex files...
    ...by removing old (and seems outdated) file detection that was in the
    Vim's config files that set filetype to the wrong `eelixir` value, which
    prevents file to be highlighted properly with nvim-treesitter.

--- dot.config/nvim/lua/stackfusion/plugins/languages.lua
+++ dot.config/nvim/lua/stackfusion/plugins/languages.lua
@@ -1,5 +1,4 @@
 return {
-  { "elixir-editors/vim-elixir", version = "*", event = "VeryLazy", },   -- e.g., provides syntax/elixir.vim
-  { "dag/vim-fish",              version = "*", event = "VeryLazy", },
-  { "vim-ruby/vim-ruby",         version = "*", event = "VeryLazy", },
+  { "dag/vim-fish",      version = "*", event = "VeryLazy", },
+  { "vim-ruby/vim-ruby", version = "*", event = "VeryLazy", },
 }
