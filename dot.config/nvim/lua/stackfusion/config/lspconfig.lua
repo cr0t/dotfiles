b0bfa2b7dd20cc5b3ea0dffe6574c58af5f41bff
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2025-01-30 10:44:17 +0100

    Shorten commit lines length to max 100 in lspconfig.lua

--- dot.config/nvim/lua/stackfusion/config/lspconfig.lua
+++ dot.config/nvim/lua/stackfusion/config/lspconfig.lua
@@ -7,14 +7,16 @@ require("mason-lspconfig").setup({
 })
 
 require("mason-lspconfig").setup_handlers({
-  -- The first entry (without a key) will be the default handler and will be called for each installed server that
-  -- doesn't have a dedicated handler.
+  -- The first entry (without a key) will be the default handler and will be called for each
+  -- installed server that doesn't have a dedicated handler.
   function(server_name)
     require("lspconfig")[server_name].setup {
       capabilities = require("cmp_nvim_lsp").default_capabilities()
     }
   end,
-  -- Next, you can provide a dedicated handler for specific servers. For example, a handler override for the `lua_ls`:
+
+  -- Next, you can provide a dedicated handler for specific servers. For example, a handler override
+  -- for the `lua_ls`:
   ["lua_ls"] = function()
     require("lspconfig").lua_ls.setup({
       settings = {
