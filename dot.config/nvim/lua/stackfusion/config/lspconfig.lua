ba5ab895cc082b99cb1c2b30148c6ea8be44bdad
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2025-06-08 23:34:44 +0200

    Re-write Neovim's lspconfig.lua for updated Mason (and Neovim itself)

--- dot.config/nvim/lua/stackfusion/config/lspconfig.lua
+++ dot.config/nvim/lua/stackfusion/config/lspconfig.lua
@@ -1,39 +1,28 @@
-require("mason").setup()
-require("mason-lspconfig").setup({
+require('mason').setup()
+
+require('mason-lspconfig').setup({
   ensure_installed = {
-    "ansiblels", "bashls", "cssls", "elixirls", "elmls", "emmet_language_server", "harper_ls",
-    "html", "lua_ls", "ruby_lsp", "svelte", "ts_ls", "yamlls"
+    'ansiblels', 'bashls', 'cssls', 'elixirls', 'elmls', 'emmet_language_server', 'harper_ls',
+    'html', 'lua_ls', 'ruby_lsp', 'svelte', 'ts_ls', 'yamlls'
   },
 })
 
-require("mason-lspconfig").setup_handlers({
-  -- The first entry (without a key) will be the default handler and will be called for each
-  -- installed server that doesn't have a dedicated handler.
-  function(server_name)
-    require("lspconfig")[server_name].setup {
-      capabilities = require("cmp_nvim_lsp").default_capabilities()
-    }
-  end,
+vim.lsp.config('emmet_language_server', {
+  filetypes = {
+    'css', 'elixir', 'eruby', 'heex', 'html', 'javascriptreact', 'less', 'pug', 'sass', 'scss',
+    'svelte', 'typescriptreact', 'vue'
+  }
+})
 
-  -- Next, you can provide a dedicated handler for specific servers. For example, a handler override
-  -- for the `lua_ls`:
-  ["lua_ls"] = function()
-    require("lspconfig").lua_ls.setup({
-      settings = {
-        Lua = {
-          diagnostics = {
-            globals = { "vim" }
-          }
-        }
-      }
-    })
-  end,
-  ["emmet_language_server"] = function()
-    require("lspconfig").emmet_language_server.setup({
-      filetypes = {
-        "css", "elixir", "eruby", "heex", "html", "javascript", "javascriptreact", "less", "sass",
-        "scss", "pug", "typescriptreact"
+vim.lsp.config('lua_ls', {
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { 'vim' }
       },
-    })
-  end,
+      telemetry = {
+        enable = false
+      }
+    }
+  }
 })
