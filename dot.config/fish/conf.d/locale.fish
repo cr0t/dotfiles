5c9507344126f8cb6ed7eec7e687973055454752
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2025-08-07 13:59:43 +0200

    Update locale.fish and set LC_ALL to simplified version
    After installing Proxmox 9 and trying to SSH into from the macOS, we
    have noticed weird warnings about locale:
    
    ```console
    -bash: warning: setlocale: LC_CTYPE: cannot change locale (UTF-8): No such file or directory
    ```
    
    In addition to that, `nvme` utility started to show temperature in
    Fahrenheit.
    
    These two issues led to make some investigation about what we're missing
    and eventually, it led to the locale settings we use for Fish on the
    client side.
    
    `LC_ALL=en_US.UTF-8` setting led to temperature units in Fahrenheit.
    
    This has been solved by going with simpler `C.UTF-8`. However, the
    `LANG` shall stay as `en_US` as preferred. We also had to explicitly set
    the `LC_CTYPE` one to fix the warnings too.

--- dot.config/fish/conf.d/locale.fish
+++ dot.config/fish/conf.d/locale.fish
@@ -1,2 +1,4 @@
-set -gx LC_ALL en_US.UTF-8
+# we need LC_* variables for SSH-ing into other machines (Debian-based, in particular)
+set -gx LC_CTYPE C.UTF-8
+set -gx LC_ALL C.UTF-8
 set -gx LANG en_US.UTF-8
