0d2317b8992ddff7dd39c18131a85b75747912d8
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2025-09-22 13:35:14 +0200

    Put ElixirLS back, and disable Emmet in Elixir files...
    ...until we get how to dynamically disable it. Emmet is quite annoying
    when typing code in the clean Elixir file and it proposes lots of
    HTML-like suggestions.

--- dot.config/nvim/lua/stackfusion/config/lspconfig.lua
+++ dot.config/nvim/lua/stackfusion/config/lspconfig.lua
@@ -2,14 +2,14 @@ require('mason').setup()
 
 require('mason-lspconfig').setup({
   ensure_installed = {
-    'ansiblels', 'bashls', 'cssls', 'elmls', 'emmet_language_server', 'expert', 'harper_ls',
+    'ansiblels', 'bashls', 'cssls', 'elixirls', 'elmls', 'emmet_language_server', 'harper_ls',
     'html', 'lua_ls', 'ruby_lsp', 'svelte', 'ts_ls', 'yamlls', 'zls'
   },
 })
 
 vim.lsp.config('emmet_language_server', {
   filetypes = {
-    'css', 'elixir', 'eruby', 'heex', 'html', 'javascriptreact', 'less', 'pug', 'sass', 'scss',
+    'css', 'eruby', 'heex', 'html', 'javascriptreact', 'less', 'pug', 'sass', 'scss',
     'svelte', 'typescriptreact', 'vue'
   }
 })
