459602059eaf9f41469d886d808fe59ca96297a8
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2025-10-08 12:08:46 +0200

    Add a couple of simple k8s aliases as fish functions

---
+++ dot.config/fish/conf.d/k8s.fish
@@ -0,0 +1,14 @@
+if type -q kubectl
+    function kpods --argument-names podsname namespace --description="Search for pods"
+        kubectl get pods -n $namespace --no-headers | grep $podsname
+    end
+
+    function kexec --argument-names podname namespace cmd --description="Execute a one-time command"
+        kubectl exec -it $podname -n $namespace -- $argv[3..]
+    end
+
+    function krails --argument-names podname namespace --description="Run Rails console"
+        set podname (kpods $podname $namespace | awk '{print $1}' | awk -F'-' 'NF==4' | head -n 1)
+        kexec $podname $namespace bundle exec rails c
+    end
+end
