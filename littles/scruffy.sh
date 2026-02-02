61ed143f70d8268d829bf24051d6161aa55ad668
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2026-02-02 17:32:26 +0100

    Update docs/links in scruffy.sh wrapper script

--- littles/scruffy.sh
+++ littles/scruffy.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
-# Scruffy Scruffington: "Scruffy's gonna clean up them processes. Yup."
+# Scruffy Scruffington mumbles, "Scruffy's gonna clean up those processes. Yup..."
 #
 # This script shall be used as a wrapper to execute long-running processes via Port to avoid
 # leaving orphaned zombie processes in case of BEAM abnormal termination. Read more:
 #
-# https://hexdocs.pm/elixir/Port.html#module-zombie-operating-system-processes
+# This script serves as a wrapper to execute long-running processes via Port and prevents creation
+# of orphaned zombie processes in the event of an abnormal termination of the BEAM VM. For further
+# information, read:
+#
+# https://hexdocs.pm/elixir/Port.html#module-orphan-operating-system-processes
 
 # Start the program in the background
 exec "$@" &
