9cb1f127ac2b9c1448126e8e70c609658f680edf
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2026-04-20 23:02:43 +0200

    Set nvim-treesitter's branch to `main`...
    ...until we figure out better solution in the given circumstances:
    [nvim-treesitter Was Archived](https://samuellawrentz.com/blog/nvim-treesitter-archived-neovim-0-12-migration/)

--- dot.config/nvim/lua/stackfusion/plugins/ide.lua
+++ dot.config/nvim/lua/stackfusion/plugins/ide.lua
@@ -3,6 +3,7 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     version = "*",
+    branch = "main",
     event = { "BufReadPre", "BufNewFile" },
     lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
     build = ":TSUpdate",
